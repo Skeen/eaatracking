d9d0c8d65df1f588a1e41f3748d806704d3017ec
drowsybeaver <andreasknielsen@gmail.com>
2013-10-03 18:48:59 +0200

    added a clear button, currently functionless

--- PhoneApp2/MainPage.xaml.cs
+++ PhoneApp2/MainPage.xaml.cs
@@ -127,10 +127,8 @@ namespace PhoneApp2
             {
                 // Get the current location and prints it.
                 var epl = e.Position.Location;
-
                 // Sends the data out
                 OutputToServer.sendData(epl.Latitude.ToString("0.000"), epl.Longitude.ToString("0.000"));
-
                 // Centers our map on the new position. 
                 map1.Center = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
 
