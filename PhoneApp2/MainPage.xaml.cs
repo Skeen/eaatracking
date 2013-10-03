5b4ebaf9f0a3cce2b09e03335d307146be18df79
drowsybeaver <andreasknielsen@gmail.com>
2013-10-03 15:40:17 +0200

    Added the pause method.

--- PhoneApp2/MainPage.xaml.cs
+++ PhoneApp2/MainPage.xaml.cs
@@ -19,21 +19,33 @@ namespace PhoneApp2
     {
         private GeoCoordinateWatcher watcher;
         private Boolean tracking = false;
+        private Boolean paused = false;
         private GeoPositionChangedEventArgs<GeoCoordinate> lastKnownLocation;
+        private DateTimeOffset startTime;
+        private DateTimeOffset pauseTime;
+
         
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
-            watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default)
-                   {
-                       MovementThreshold = 5
-                   };
-            watcher.PositionChanged += this.watcher_PositionChanged;
-            watcher.PositionChanged += this.watcher_SpeedLevels;
+            setUpRandomThings();
         }
 
+        // To be called when our app starts, and setup most of the things.
+        private void setUpRandomThings()
+        {
+            watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default)
+            {
+                MovementThreshold = 5
+            };
+            
+            watcher.PositionChanged += this.watcher_PositionChanged;
+            watcher.PositionChanged += this.watcher_SpeedLevels;
+
+            stopButton.Opacity = 0.5;
+            pauseButton.Opacity = 0.5;
+        }
 
         /* Handle the information given when the start button is pressed. 
          * Currently starting the tracking */
@@ -45,7 +57,31 @@ namespace PhoneApp2
                 watcher.Start();
                 tracking = true;
                 stopButton.Opacity = 1.0;
+                pauseButton.Opacity = 1.0;
                 startButton.Opacity = 0.5;
+            }
+        }
+
+        /* Handle the information given when the pause button is pressed.
+         * Currently pausing the run, and enable the user to resume when clicked again */
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tracking)
+            {
+                if (paused)
+                {
+                    startTime.Add(DateTimeOffset.Now.Subtract(pauseTime));
+                    pauseButton.Content = "Pause";
+                    infoBlock.Text += "Run resumed\n" + startTime.TimeOfDay + "\n";
+                    paused = false;
+                }
+                else
+                {
+                    pauseTime = DateTimeOffset.Now;
+                    paused = true;
+                    pauseButton.Content = "Resume";
+                    infoBlock.Text += "Run paused\n" + startTime.TimeOfDay + "\n";
+                }
             }
         }
 
@@ -59,6 +95,7 @@ namespace PhoneApp2
                 watcher.Stop();
                 tracking = false;
                 stopButton.Opacity = 0.5;
+                pauseButton.Opacity = 0.5;
                 startButton.Opacity = 1.0;
             }
         }
@@ -66,40 +103,44 @@ namespace PhoneApp2
         // Method to change your position on the map, when the phone move positon.
         private void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
+            // Checks if we actually have a position, otherwise shows a message.
             if (e.Position.Location.IsUnknown)
             {
                 MessageBox.Show("Please wait while your prosition is determined....");
                 return;
             }
+            // Get the current location and prints it.
             var epl = e.Position.Location;
             textBlock1.Text = textBlock1.Text + epl.Latitude.ToString("0.000") + "\t" + epl.Longitude.ToString("0.000") + "\n";
 
+            // Sends the data out
             OutputToServer.sendData(epl.Latitude.ToString("0.000"), epl.Longitude.ToString("0.000"));
-
+            
+            // Centers our map on the new position. 
             map1.Center = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
-
+            
             if (map1.Children.Count != 0)
             {
                 var pushpin = map1.Children.FirstOrDefault(p => (p.GetType() == typeof(Pushpin) && (string)((Pushpin)p).Tag == "locationPushpin"));
-
                 if (pushpin != null)
                 {
                     map1.Children.Remove(pushpin);
                 }
             }
 
+            // Show our location with a pin on the map.
             Pushpin locationPushpin = new Pushpin();
             locationPushpin.Tag = "locationPushpin";
             locationPushpin.Location = watcher.Position.Location;
             map1.Children.Add(locationPushpin);
             map1.SetView(watcher.Position.Location, 16.0);
-
         }
 
         /* Method to handle the speed of which our runner (walker) is moving.
          * Also handle the zoom level of the map. */
         private void watcher_SpeedLevels(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
+            // Checks if we actually have a position, otherwise shows a message.
             if (e.Position.Location.IsUnknown)
             {
                 MessageBox.Show("Please wait while your speed is determined....");
@@ -108,22 +149,27 @@ namespace PhoneApp2
 
             if (lastKnownLocation != null)
             {
-                // Random funtions to hopefully clean up the code (somewhat)
+                // Random functions to clean up the code (somewhat)
                 var oldLocation = lastKnownLocation.Position.Location;
                 var oldTime     = lastKnownLocation.Position.Timestamp;
                 var newLocation = e.Position.Location;
                 var newTime     = e.Position.Timestamp;
                 
+                // Calculate the difference between this and the preivous location.
                 var distance = oldLocation.GetDistanceTo(newLocation);
                 var time = newTime.Subtract(oldTime);
 
+                // Calculate the speed and adjusts the map accordingly. 
                 var speed = Math.Round(distance / time.TotalSeconds);
                 var speedKm = speed * 3.6;
-                speedBox.Text = speed.ToString() + " m/sec\n" + speedKm.ToString() + " km/h";
+                speedBlock.Text = "Current speed: " + speed.ToString() + " m/sec\n" + speedKm.ToString() + " km/h";
                 map1.ZoomLevel = 20 - (speedKm * 0.3);
             }
             
+            // If no position was found, its the first reading and we have nothing to compare to
+            // So we add this location, and are ready for the next move.
             lastKnownLocation = e;
+            startTime = e.Position.Timestamp;
         }
     }
 }
