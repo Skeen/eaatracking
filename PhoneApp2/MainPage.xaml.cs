f5734d6ec5c5fd89fec7b035d1d18d64b382e593
Emil Madsen <skeen@cs.au.dk>
2013-10-17 17:21:38 +0200

    Magic updates

--- PhoneApp2/MainPage.xaml.cs
+++ PhoneApp2/MainPage.xaml.cs
@@ -152,11 +152,13 @@ namespace PhoneApp2
                 handleRunRoute(e);
                 
                 // Sends the data out
+                /*
                 var ePosLatitude = e.Position.Location.Latitude;
                 var ePosLongitude = e.Position.Location.Longitude;
                 DateTimeOffset timeStamp = e.Position.Timestamp.DateTime;
                 PositionInformation pi = new PositionInformation(timeStamp, ePosLatitude, ePosLongitude);
                 OutputToServer.sendData("rundt_om_nygaard", pi);
+                */
                 // Centers our map on the new position. 
                 map1.Center = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
 
@@ -249,7 +251,7 @@ namespace PhoneApp2
 
         private void uploadButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Content = new SavePage();
+            this.Content = new SavePage(currentRunPositions);
         }
     }
 }
