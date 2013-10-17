d4bea8216dcb6b32589a9525efc7512fbdf8bc8d
Emil Madsen <skeen@cs.au.dk>
2013-10-17 16:28:26 +0200

    Yo dawg, I herd ya like callbacks, so we put a callback inside a callback, which schedules a callback.

---
+++ PhoneApp2/LoadPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Collections.ObjectModel;
+
+namespace PhoneApp2
+{
+    public partial class LoadPage : PhoneApplicationPage
+    {
+        ObservableCollection<String> obs;
+
+        public LoadPage()
+        {
+            InitializeComponent();
+            OutputToServer.listRoutes(callback);
+        }
+
+        public void callback(List<string> routeIDs)
+        {
+            System.Windows.Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                obs = new ObservableCollection<string>(routeIDs);
+                lstbx1.DataContext = obs;
+            });
+        }
+
+        private void back_clicked(object sender, RoutedEventArgs e)
+        {
+            this.Content = new MainPage();
+        }
+    }
+}
